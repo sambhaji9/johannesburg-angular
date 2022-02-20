8292ee0c8f7db5fadeb278fa667d78c06a4d668f
Sambhaji Mane <mane.sambhaji9@gmail.com>
2022-02-20 13:23:10 +0530

    Styled the list with table, removed list-item component

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
-import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
-  declarations: [TasksListComponent, ListItemComponent],
+  declarations: [TasksListComponent],
   imports: [
     CommonModule
   ],
